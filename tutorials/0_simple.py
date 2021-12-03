84e40fcb99b89f3eb969300fd7b1963f79d7d8d5
BlueWorldInc <keamybakunin@gmail.com>
2021-12-03 17:07:25 +0100

    working on centering the window

--- tutorials/0_simple.py
+++ tutorials/0_simple.py
@@ -10,13 +10,45 @@ class Example(QtWidgets.QWidget):
 		self.initUI()
 
 	def initUI(self):
+		
+		# ToolTips
+		# QtWidgets.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+		# self.setToolTip('This is a <b>QWidget</b> widget')
+		# btn = QtWidgets.QPushButton('Button', self)
+		# btn.resize(btn.sizeHint())
+		# btn.move(50, 50)
+		# btn.setToolTip('This is a <b>QPushButton</b> widget')
+
+		# Exit button
+		qbtn = QtWidgets.QPushButton('Quit', self)
+		qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+
 		self.setGeometry(300, 300, 250, 150)
 		self.setWindowTitle('Icon')
 		note_icons_file_path = "C:\\Users\\neo\\Documents\\CODE\\Python\\Noteman\\gui\\"
 		app_icon = QtGui.QIcon()
 		app_icon.addFile(note_icons_file_path + 'notes16x16.png', QtCore.QSize(16,16))
 		self.setWindowIcon(app_icon)
+		self.center()
 		self.show()
+
+	def closeEvent(self, event):
+		reply = QtWidgets.QMessageBox.question(self, 'Message',
+	    "Are you sure to quit?", QtWidgets.QMessageBox.Yes | 
+    	QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
+
+		if reply == QtWidgets.QMessageBox.Yes:
+		    event.accept()
+		else:
+		    event.ignore()
+	
+	def center(self):
+		qr = self.frameGeometry()
+		cp = QtGui.QScreen().availableGeometry().center()
+		qr.moveCenter(cp)
+		# self.move(qr.topLeft())
+		# self.move(qr.topLeft())
+	
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
